b3079ee1da8b6083a8ddc40063b9f96b14533fc0
Shelly Ambar <42389001+ShellyAmbar@users.noreply.github.com>
2024-08-27 17:47:37 +0300

    adding welcome screen

--- screens/welcome-screen/welcome-screen.styles.ts
+++ screens/welcome-screen/welcome-screen.styles.ts
@@ -12,7 +12,8 @@ export default StyleSheet.create({
   },
   startBtn: {
     alignSelf: 'center',
-    backgroundColor: GlobalColors.Brand.primary,
+
+    backgroundColor: GlobalColors.Shades.primary,
     zIndex: 3,
     width: '50%',
     paddingVertical: 20,
@@ -20,23 +21,57 @@ export default StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  disabledBtn: {
+    backgroundColor: GlobalColors.Brand.primary,
+  },
+  disabledText: {
+    color: GlobalColors.Shades.primary,
+  },
   title: {
     color: GlobalColors.TextColors.white,
+
     zIndex: 3,
-    fontSize: 30,
+    fontSize: 40,
     marginBottom: 20,
+    fontFamily: 'Jersey10-Regular',
   },
   subTitle: {
-    color: GlobalColors.Brand.secondary,
+    color: GlobalColors.Brand.thierd,
     zIndex: 3,
     fontSize: 20,
     marginBottom: 50,
     textAlign: 'center',
+    fontFamily: 'Jersey10-Regular',
   },
-  btnText: {
+  textInputContainer: {
+    borderColor: GlobalColors.TextColors.white,
+    borderWidth: 1,
+    borderRadius: 10,
+
+    width: '80%',
+    zIndex: 3,
+    marginBottom: 50,
+  },
+  textInputText: {
     color: GlobalColors.TextColors.white,
+    fontFamily: 'Jersey10-Regular',
     fontSize: 20,
     textAlign: 'center',
+  },
+  text: {
+    color: GlobalColors.TextColors.white,
+    zIndex: 3,
+    fontSize: 16,
+    marginBottom: 10,
+    textAlign: 'center',
+    fontFamily: 'Jersey10-Regular',
+  },
+  btnText: {
+    color: GlobalColors.Brand.primary,
+
+    fontSize: 20,
+    textAlign: 'center',
+    fontFamily: 'Jersey10-Regular',
   },
   video: {
     position: 'absolute',
--- screens/welcome-screen/welcome-screen.tsx
+++ screens/welcome-screen/welcome-screen.tsx
@@ -1,10 +1,13 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, Keyboard} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import styles from './welcome-screen.styles';
 import {MediaStream, RTCView} from 'react-native-webrtc';
 import {getStream} from '@hive/utils/stream-util';
+import ReactiveTextInput from 'rn-reactive-text-input';
+import {GlobalColors} from '@hive/styles/colors';
 const WelcomeScreen = props => {
   const [localStream, setLocalStream] = useState<null | MediaStream>();
+  const [name, setName] = useState('');
   useEffect(() => {
     (async () => {
       const stream = await getStream(false, true);
@@ -16,6 +19,25 @@ const WelcomeScreen = props => {
     <View style={styles.container}>
       <Text style={styles.title}>DateRoulatte</Text>
       <Text style={styles.subTitle}>Meet new friends in one click away ..</Text>
+      <Text style={styles.text}>Enter your name..</Text>
+
+      <ReactiveTextInput
+        containerStyle={styles.textInputContainer}
+        textInputStyle={styles.textInputText}
+        textContentType="familyName"
+        onDebounce={text => {
+          setName(text);
+        }}
+        keyboardType="email-address"
+        keyboardAppearance="default"
+        blurOnSubmit={false}
+        autoCorrect={false}
+        returnKeyType="done"
+        onSubmitEditing={() => {
+          Keyboard.dismiss();
+        }}
+        cursorColor={GlobalColors.TextColors.white}
+      />
       <RTCView
         streamURL={localStream?.toURL()}
         objectFit="cover"
@@ -23,11 +45,15 @@ const WelcomeScreen = props => {
       />
       <View style={styles.shade} />
       <TouchableOpacity
-        style={styles.startBtn}
+        disabled={name?.length === 0}
+        style={[styles.startBtn, name?.length === 0 && styles.disabledBtn]}
         onPress={() => {
           props.navigation.navigate('Home');
         }}>
-        <Text style={styles.btnText}>Start</Text>
+        <Text
+          style={[styles.btnText, name?.length === 0 && styles.disabledText]}>
+          Start
+        </Text>
       </TouchableOpacity>
     </View>
   );
