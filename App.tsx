1481fe95118b0274a5356d7cf2540012643c3efe
Shelly Ambar <42389001+ShellyAmbar@users.noreply.github.com>
2024-09-03 04:17:36 +0300

    release

--- App.tsx
+++ App.tsx
@@ -9,13 +9,12 @@ import {NavigationContainer} from '@react-navigation/native';
 
 import React from 'react';
 
-import {LogBox, SafeAreaView, StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
 import StackNavigation from './navigation/stack-navigation';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from './store';
 
-LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
